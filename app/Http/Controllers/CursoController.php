3f1cb6aa189a81cacc7a54ed8a0f6c9ee4ecf031
azaelchel <azael_pece@hotmail.com>
2025-03-25 18:02:58 -0600

    tarea

---
+++ app/Http/Controllers/CursoController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CursoController extends Controller
+{
+    public function getNames(){
+        return view('welcome', ['name' => 'Azael']);
+    }
+
+    public function suma($a, $b)
+    {
+        $resultado = $a + $b;
+        return "La suma de $a + $b es: $resultado";
+    }
+
+    public function resta($a, $b)
+    {
+        $resultado = $a - $b;
+        return "La resta de $a - $b es: $resultado";
+    }
+
+    public function multiplicacion($a, $b)
+    {
+        $resultado = $a * $b;
+        return "La multiplicación de $a * $b es: $resultado";
+    }
+
+    public function division($a, $b)
+    {
+        if ($b == 0) {
+            return "No se puede dividir entre cero.";
+        }
+
+        $resultado = $a / $b;
+        return "La división de $a / $b es: $resultado";
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\CursoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [CursoController::class, 'getNames'])->name('home');
+
+Route::get('/suma/{a}/{b}', [CursoController::class, 'suma'])->name('suma');
+Route::get('/resta/{a}/{b}', [CursoController::class, 'resta'])->name('resta');
+Route::get('/multiplicacion/{a}/{b}', [CursoController::class, 'multiplicacion'])->name('multiplicacion');
+Route::get('/division/{a}/{b}', [CursoController::class, 'division'])->name('division');
+
