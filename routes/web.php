4f407567f05004ee59b7900451b6cdc8b936c90b
azaelchel <azael_pece@hotmail.com>
2025-03-26 16:23:00 -0600

    tarea 2

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [CursoController::class, 'getNames'])->name('home');
 
+
+
 Route::get('/suma/{a}/{b}', [CursoController::class, 'suma'])->name('suma');
 Route::get('/resta/{a}/{b}', [CursoController::class, 'resta'])->name('resta');
 Route::get('/multiplicacion/{a}/{b}', [CursoController::class, 'multiplicacion'])->name('multiplicacion');
